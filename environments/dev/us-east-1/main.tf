79dc30dcb21504eb4eff650528d8b18d3d30a3cf
Jerry Hoopfer <jerry.hoopfer@gmail.com>
2026-02-16 13:57:27 -0500

    backend

--- environments/dev/us-east-1/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "scale-solutions-terraform-state-dev"
-    key            = "platform/dev/us-east-1/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-locks-dev"
-  }
-}
