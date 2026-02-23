6846268600805ef2f787fea04eae3787238009ed
jerry hoopfer <jerry.hoopfer@gmail.com>
2026-02-22 21:50:45 -0500

    updatte based on code review feedback

--- environments/dev/us-east-1/01-compute/load_balancer_internal.tf
+++ environments/dev/us-east-1/01-compute/load_balancer_internal.tf
@@ -8,13 +8,13 @@ resource "aws_security_group" "alb_internal" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.100.0.0/20"] # Replace with your VPC CIDR
+    cidr_blocks = [data.terraform_remote_state.network.outputs.vpc_cidr_block]
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["10.100.0.0/20"]
+    cidr_blocks = [data.terraform_remote_state.network.outputs.vpc_cidr_block]
   }
   egress {
     from_port   = 0
